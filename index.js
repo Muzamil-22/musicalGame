5a54e1acd0034449ecea3d2e046082d2ef5de2ed
Muzamil-22 <102140354+Muzamil-22@users.noreply.github.com>
2022-04-05 16:26:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function dru() {
-     this.style.color="white";
-    var butt=this.innerHTML;
-    switch (butt) {
+    for(let i=0;i<=6;i++){
+        document.querySelectorAll("button")[i].addEventListener("click", function dru() {
+            this.style.color="white";
+           var butt=this.innerHTML;   
+           sound(butt);   
+           anime(butt);     
+           });
+}
+
+document.addEventListener("keypress", function(event) {
+    sound(event.key);
+    anime(event.key);
+});
+    
+function sound(key) {
+    switch (key) {
         case "w":
             var tom1= new Audio("sounds/tom-1.mp3");
             tom1.play();           
@@ -24,17 +36,22 @@ function dru() {
             break;
         case "k":
             var kick= new Audio("sounds/kick-bass.mp3");
-            kick.play(); 
+            kick.play()
             break;
         case "l":
             var snare= new Audio("sounds/snare.mp3");
             snare.play(); 
             break;
-        default: alert("sheat! here we go again");
+        // default: alert("sheat! here we go again");
     }
- 
-}
-for(let i=0;i<=6;i++){
-document.querySelectorAll("button")[i].addEventListener("click",dru);
 }
 
+function anime(sameKey) {
+    var but = document.querySelector("."+sameKey);
+    but.classList.add("pressed");
+
+    setTimeout(function() {
+        but.classList.remove("pressed"),100
+    })
+
+}
